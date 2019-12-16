7ee3277705f5a9c79e8cafd080da67a40c77a4c7
Hex-Zero <hex0@live.com>
2019-12-16 17:00:02 +0000

    I give up

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,40 +1,39 @@
 import { Link } from "gatsby"
 import React from "react"
-import Logo from "../images/mainLogo.svg"
 
-const Header = ({ siteTitle }) => (
-  <header>
-    <Link activeClassName="active" to="/">
-      <Logo />
-    </Link>
-    <nav>
-      <h1>
-        <Link activeClassName="active" to="/">
-          Main
-        </Link>
-      </h1>
-      <h1>
-        <Link activeClassName="active" to="/about">
-          About
-        </Link>
-      </h1>
-      <h1>
-        <Link activeClassName="active" to="/contact">
-          Contact
-        </Link>
-      </h1>
-      <h1>
-        <Link activeClassName="active" to="/gallery">
-          Gallery
-        </Link>
-      </h1>
-      <h1>
-        <Link activeClassName="active" to="/pricing">
-          Pricing
-        </Link>
-      </h1>
-    </nav>
-  </header>
-)
+const Header = ({ siteTitle }) => {
+  return (
+    <header>
+      <Link activeClassName="active" to="/"></Link>
+      <nav>
+        <h1>
+          <Link activeClassName="active" to="/">
+            Main
+          </Link>
+        </h1>
+        <h1>
+          <Link activeClassName="active" to="/about">
+            About
+          </Link>
+        </h1>
+        <h1>
+          <Link activeClassName="active" to="/contact">
+            Contact
+          </Link>
+        </h1>
+        <h1>
+          <Link activeClassName="active" to="/gallery">
+            Gallery
+          </Link>
+        </h1>
+        <h1>
+          <Link activeClassName="active" to="/pricing">
+            Pricing
+          </Link>
+        </h1>
+      </nav>
+    </header>
+  )
+}
 
 export default Header
